7918401c8ec9edafc39cb4d02776d444e951d04e
Berenicio <bernardo.ssilva0808@gmail.com>
2023-05-24 19:24:38 -0300

    Aplied Cross origin

--- src/main/java/com/example/cardapio/Controllers/FoodController.java
+++ src/main/java/com/example/cardapio/Controllers/FoodController.java
@@ -3,6 +3,7 @@ package com.example.cardapio.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,12 +24,14 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<FoodDTO> getAll() {
         List<FoodDTO> foodList = repository.findAll().stream().map(FoodDTO::new).toList();
         return foodList;
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
     public void saveFood(@RequestBody FoodRequestDTO data) {
         Food foodData = new Food(data);
