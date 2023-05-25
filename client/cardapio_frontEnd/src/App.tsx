7ef773dee4354be5ed58bff1d90287d223d72f3c
Berenicio <bernardo.ssilva0808@gmail.com>
2023-05-25 11:52:29 -0300

    changed the visual of app

--- client/cardapio_frontEnd/src/App.tsx
+++ client/cardapio_frontEnd/src/App.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import { Card } from './components/card/card';
+import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 function App() {
-  
+  const {data} = useFoodData();
+  return (
+    <div class="container">
+      <h1>Cardapio</h1>
+      <div className="card-grid">
+        {data?.map(foodData => 
+        <Card 
+          price={foodData.price} 
+          image={foodData.image} 
+          name={foodData.name}
+        />)}
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ client/cardapio_frontEnd/src/components/card/card.tsx
@@ -0,0 +1,22 @@
+import "./card.css"
+
+interface CardProps{
+    price: number,
+    name : string,
+    image: string
+}
+
+export function Card(price, image, name: CardProps){
+    return(
+        <div className="card">
+            <img src={image} alt="" />
+            <h2>
+                {title}
+            </h2>
+
+            <p>
+                <b>Valor: </b> {price}
+            </p>
+        </div>
+    )
+}
